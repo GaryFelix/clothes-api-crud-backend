ab78f8f26168eac3dce3265fe9abcf4bebdf0697
GaryFelix <2k19cse020@kiot.ac.in>
2024-06-14 18:06:11 +0530

    update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 
 
 //mongoDB Connection
-mongoose.connect("mongodb+srv://2k19cse020:xm2NJ7d9Whc52mDj@backenddbapi.9s19jxg.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDBAPI").then(() => {
+mongoose.connect("mongodb+srv://2k19cse020:xm2NJ7d9Whc52mDj@backenddbapi.9s19jxg.mongodb.net/Node-API?retryWrites=true&w=majority").then(() => {
     console.log("Connected to DB");
     app.listen(PORT, () => {
         console.log(`Port is listening on ${PORT}`);
